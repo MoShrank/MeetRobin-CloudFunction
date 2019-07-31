9171768dba0a0c97d44afeff2b847d7e35606e8b
Moritz Eich <moritz.eich@joblift.de>
2019-07-31 19:33:21 +0200

    production prototype

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,73 @@
-const functions = require("firebase-functions");
+vconst functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.updateTypeInfo = functions.database.ref("user/{userId}").onWrite(event => {
-    const getSomethingPromise = admin.database().ref(`/user/{userId}/answers`).once('value');
-    getSomethingPromise.then(results => {
-        let answers = []
-        Object.entries(answers).forEach(
-            ([key, value]) => answers.push(value) 
-        );
-        let score = calculateScore(answers, 0);
+exports.updateTypeInfo = functions.database.ref(`/users/{userId}/answers`).onWrite((snapshot, context) => {
+      console.log("starting to calculate score");
+      let user_id = context.params.userId;
+      let results = snapshot.after.val();
+      let answers = [];
+      let keys = Object.keys(results);
+      let totalQuestions = 5;
+      let totalQuestionsAmount = 20;
+      // { q1: {answer: 1} }
 
-        //update values
+      for(let i = 0; i < keys.length; i++) {
+        answers.push(results[keys[i]]);
+      }
+      let scores = [];
+      let dimension = ["A", "B", "C", "D"];
 
-    })
+      //calculates score of questionas and pushes it in array
+      for(let i = 0; i < 4; i++) {
+        let tempAnswers = answers.filter(answer => answer.dimension === dimension[i]);
+        let score = calculateScore(tempAnswers, totalQuestions);
+        scores.push(score);
+      }
+      let scoreIndex = getMaxIndexFromArray(scores);
+      console.log(`score: ${scores[scoreIndex]} and type: ${dimension[scoreIndex]}`)
+      let db = admin.database();
+      let ref = db.ref(`/users/${user_id}`);
+      let user_ref = ref.child("profile");
+      user_ref.set({
+        score: scores[scoreIndex],
+        type: dimension[scoreIndex],
+        accuracy: calculateAccuracy(totalQuestionsAmount, answers.length)
+      });
 
-    
+      return 0;
 });
 
-function calculateScore(answeredQuestion, totalQuestions) {
+function getMaxIndexFromArray(array) {
+  var max = array[0];
+  var maxIndex = 0;
+
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+          maxIndex = i;
+          max = array[i];
+      }
+  }
+  return maxIndex;
+}
+
+function calculateScore(answeredQuestions, totalQuestions) {
     let positiveAnsweredQuestions = 0;
-    for(let i = 0; answeredQuestions.length < i; i++) {
-      if (answeredQuestion.value == 1) {
+    for(let i = 0; i < answeredQuestions.length; i++) {
+        if (answeredQuestions[i].value === 1) {
         positiveAnsweredQuestions += 1;
       }
     }
     let score = (positiveAnsweredQuestions / totalQuestions) * 100;
+    console.log(`calculated score: ${score}`);
     return score;
   }
 
-
-//calculate accuray using weird formula
-function calculateAccuracy() {
-
+function countQuestions() {
+  return [5, 5, 5, 5];
 }
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//calculate accuray using weird formula
+function calculateAccuracy(totalQuestionsAmount, answeredQuestionsAmount) {
+  return (answeredQuestionsAmount / totalQuestionsAmount) * 100;
+}
