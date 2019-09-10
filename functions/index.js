c4c5434f029c84c5f2997ebea66b2aaaf015453d
Moritz Eich <moritz.eich@joblift.de>
2019-09-10 19:38:48 +0200

    fixes

--- functions/index.js
+++ functions/index.js
@@ -1,73 +1,71 @@
-vconst functions = require("firebase-functions");
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 exports.updateTypeInfo = functions.database.ref(`/users/{userId}/answers`).onWrite((snapshot, context) => {
-      console.log("starting to calculate score");
-      let user_id = context.params.userId;
-      let results = snapshot.after.val();
-      let answers = [];
-      let keys = Object.keys(results);
-      let totalQuestions = 5;
-      let totalQuestionsAmount = 20;
-      // { q1: {answer: 1} }
+    const dimension = ["A", "B", "C", "D"];
 
-      for(let i = 0; i < keys.length; i++) {
+    console.log("starting to calculate score");
+    let user_id = context.params.userId;
+    let results = snapshot.after.val();
+    let answers = [];
+
+    let totalQuestions = 5;
+    let totalQuestionsAmount = 20;
+    // { q1: {answer: 1} }
+
+    let keys = Object.keys(results);
+    for (let i = 0; i < keys.length; i++) {
         answers.push(results[keys[i]]);
-      }
-      let scores = [];
-      let dimension = ["A", "B", "C", "D"];
+    }
+    let scores = [];
 
-      //calculates score of questionas and pushes it in array
-      for(let i = 0; i < 4; i++) {
-        let tempAnswers = answers.filter(answer => answer.dimension === dimension[i]);
+    //calculates score of questions and pushes it in array
+    dimension.forEach((element) => {
+        let tempAnswers = answers.filter(answer => answer.dimension === element);
         let score = calculateScore(tempAnswers, totalQuestions);
         scores.push(score);
-      }
-      let scoreIndex = getMaxIndexFromArray(scores);
-      console.log(`score: ${scores[scoreIndex]} and type: ${dimension[scoreIndex]}`)
-      let db = admin.database();
-      let ref = db.ref(`/users/${user_id}`);
-      let user_ref = ref.child("profile");
-      user_ref.set({
+    });
+
+    let scoreIndex = getMaxIndexFromArray(scores);
+    console.log(`score: ${scores[scoreIndex]} and type: ${dimension[scoreIndex]}`);
+    let db = admin.database();
+    let ref = db.ref(`/users/${user_id}/profile`);
+    ref.set({
         score: scores[scoreIndex],
         type: dimension[scoreIndex],
         accuracy: calculateAccuracy(totalQuestionsAmount, answers.length)
-      });
+    }).catch( () => {
+        console.log(`could not update user profile information`);
+    });
 
-      return 0;
+    return 0;
 });
 
 function getMaxIndexFromArray(array) {
-  var max = array[0];
-  var maxIndex = 0;
+    let max = array[0];
+    let maxIndex = 0;
 
-  for (let i = 1; i < array.length; i++) {
-      if (array[i] > max) {
-          maxIndex = i;
-          max = array[i];
-      }
-  }
-  return maxIndex;
+    array.forEach((element, index) => {
+        if (element > max) {
+            maxIndex = index;
+        }
+    });
+    return maxIndex;
 }
 
 function calculateScore(answeredQuestions, totalQuestions) {
     let positiveAnsweredQuestions = 0;
-    for(let i = 0; i < answeredQuestions.length; i++) {
+    for (let i = 0; i < answeredQuestions.length; i++) {
         if (answeredQuestions[i].value === 1) {
-        positiveAnsweredQuestions += 1;
-      }
+            positiveAnsweredQuestions += 1;
+        }
     }
     let score = (positiveAnsweredQuestions / totalQuestions) * 100;
     console.log(`calculated score: ${score}`);
     return score;
-  }
-
-function countQuestions() {
-  return [5, 5, 5, 5];
 }
 
-//calculate accuray using weird formula
 function calculateAccuracy(totalQuestionsAmount, answeredQuestionsAmount) {
-  return (answeredQuestionsAmount / totalQuestionsAmount) * 100;
+    return Math.round((answeredQuestionsAmount / totalQuestionsAmount) * 100);
 }
